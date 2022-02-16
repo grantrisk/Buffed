3e09a01a1e5b85353ffcdeef27e8db79d22786f8
Blake Jackson <baj8533@uncw.edu>
2022-02-15 21:42:25 -0500

    Minor Text Edits.

--- Buffed/src/app.py
+++ Buffed/src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Welcome to Buffed! \n Blake Jackson'
+    return 'Welcome to Buffed! Blake Jackson'
 
 
 if __name__ == '__main__':
