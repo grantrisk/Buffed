62930444537ce0a2a9f202ef83fdcf3d8fd3489b
cody-jackson22 <98300205+cody-jackson22@users.noreply.github.com>
2022-02-16 22:52:12 -0500

    Added my name to home file

--- Buffed/src/app.py
+++ Buffed/src/app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 
-    return '<h1>Welcome to Buffed!</h1><br> Created by Blake Jackson, Tabs Chemelli, Grant Risk, Steven Allen, Cody Jackson'
+    return '<h1>Welcome to Buffed!</h1><br> Created by Blake Jackson, Tabs Chemelli, Grant Risk, Steven Allen, ' \
+           'Cody Jackson'
 
 
 if __name__ == '__main__':
