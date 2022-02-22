2fc4b597f429488810501e60a432114c31a17983
Blake Jackson <baj8533@uncw.edu>
2022-02-21 21:29:40 -0500

    Relocated templates folder, added nav for pages.

--- Buffed/src/app.py
+++ Buffed/src/app.py
@@ -1,14 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
+    return render_template('index.html')
 
-    return '<h1>Welcome to Buffed!</h1><br> Created by Blake Jackson, Tabs Chemelli, Grant Risk, Steven Allen, ' \
-           'Cody Jackson'
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/find_meals')
+def find_meals():
+    return render_template('find_meals.html')
+
+@app.route('/my_meals')
+def my_meals():
+    return render_template('my_meals.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/my_goals')
+def my_goals():
+    return render_template('my_goals.html')
+
+@app.route('/my_profile')
+def my_profile():
+    return render_template('my_profile.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
