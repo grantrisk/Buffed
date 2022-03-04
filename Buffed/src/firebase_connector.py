a7724354cae87cff46d479663ef77846b477ebbe
Tabs Chemelli <tabchemelli@hotmail.com>
2022-03-04 10:21:38 -0500

    Updated About

--- Buffed/src/firebase_connector.py
+++ Buffed/src/firebase_connector.py
@@ -1,5 +1,10 @@
 from typing import List
 from models import Meal, User
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+project_id = 'buffed-9aca2'
 
 
 class FirebaseConnector:
