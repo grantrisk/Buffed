18c78b13dc7f2d2fec3456784fce650b9d2b13f5
Tabs Chemelli <tabchemelli@hotmail.com>
2022-03-25 00:36:31 -0400

    Added Message Flashing, alert styling, contact form availability flag and validation; Completes #27, #5

--- Buffed/src/blueprints/about.py
+++ Buffed/src/blueprints/about.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
-from src.forms import ContactForm
+from Buffed.src.forms import ContactForm
 
 about_page = Blueprint("about", __name__, static_folder="static", template_folder="templates")
 
@@ -15,16 +15,25 @@ def send_contact(result):
     # This is for a User Story that's in the icebox currently (noted 3/17/2022)
     pass
 
+
 @about_page.route('/', methods=["GET", "POST"])
 def about():
     """
         This method returns the about us page and handles any posted contact form info.
-        :return: render_template('about.html')
+        :return: renders template for about.html with contact form
     """
+    # flag if the contact form is available
+    available = False
     form = ContactForm()
 
-    if request.method == 'POST':
-        # if an email is posted, store email information as result
+    if not available:
+        # flash message to user about unavailable contact form
+        flash("Sorry, our contact form is not ready to use yet!", "error")
+
+    # validate_on_submit checks for submission with POST method,
+    # then calls validate() to trigger form validation
+    if form.validate_on_submit():
+        # if a contact form is posted, store email information as result
         result = {'name': request.form["name"],
                   'email': request.form["email"],
                   'subject': request.form["subject"],
@@ -33,7 +42,8 @@ def about():
         # Send this email result using send_contact method
         send_contact(result)
 
-        # resume as normal (show about.html page with form after sending)
+        # resume as normal (re-render about.html page with form after sending)
         return render_template('about.html', form=form)
     else:
+        # render template with contact form
         return render_template('about.html', form=form)
--- Buffed/src/forms.py
+++ Buffed/src/forms.py
@@ -1,10 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 
 class ContactForm(FlaskForm):
-    name = StringField("Name")
-    email = EmailField("Email")
-    subject = StringField("Subject")
-    message = TextAreaField("Message")
+    """
+    Creates contact form with validation specifications
+    """
+    name = StringField("Name", validators=[DataRequired()])
+    email = EmailField("Email", validators=[Email()])
+    subject = StringField("Subject", validators=[DataRequired()])
+    message = TextAreaField("Message", validators=[DataRequired()])
     submit = SubmitField("Send")
