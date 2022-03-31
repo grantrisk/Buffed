c8b60d6ba94319f293098ff52d58b92d407c9150
grantrisk <riskgrant@gmail.com>
2022-03-31 17:20:28 -0400

    fixed error caused by model refactor in todays_plan.py

--- Buffed/src/blueprints/todays_plan.py
+++ Buffed/src/blueprints/todays_plan.py
@@ -4,9 +4,9 @@ from src.models import Meal
 todays_plan_page = Blueprint("todays_plan", __name__, static_folder="static", template_folder="templates")
 
 
-meal1 = Meal("burger", "1", {"calories":400, "protein":20, "carbs":250, "fat":50}, [], [], "Dinner")
-meal2 = Meal("yogurt", "2", {"calories":100, "protein":5, "carbs":90, "fat":40}, [], [], "Breakfast")
-meal3 = Meal("pizza", "3", {"calories":300, "protein":25, "carbs":200, "fat":80}, [], [], "Lunch")
+meal1 = Meal("burger", "1", "", {"calories":400, "protein":20, "carbs":250, "fat":50}, [], [], [])
+meal2 = Meal("yogurt", "2", "", {"calories":100, "protein":5, "carbs":90, "fat":40}, [], [], [])
+meal3 = Meal("pizza", "3", "", {"calories":300, "protein":25, "carbs":200, "fat":80}, [], [], [])
 meals = [meal1, meal2, meal3]
 
 def delete_meal(meal: Meal):
