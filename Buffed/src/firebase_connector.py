50e7f0a7893653ea821562753c56779ae09b02ef
grantrisk <riskgrant@gmail.com>
2022-04-09 11:30:50 -0400

    refacrtored firebase_connector.py

--- Buffed/src/firebase_connector.py
+++ Buffed/src/firebase_connector.py
@@ -14,48 +14,6 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-def create_firebase_account(email, password):
-    '''
-
-    '''
-    user = firebase_admin.auth.create_user(email=email, password=password)
-    userUID = user.uid
-    data = {
-        u'birth': "",
-        u'current_goal': "",
-        u'email': email,
-        u'gender': "",
-        u'height': "",
-        u'name': "",
-        u'weight': "",
-    }
-    db.collection(u'users').document(userUID).set(data)
-
-
-def sign_in_with_email_and_password(email: str, password: str):
-    payload = json.dumps({
-        "email": email,
-        "password": password,
-        "returnSecureToken": "true"
-    })
-    r = requests.post(rest_api_url,
-                      params={"key": FIREBASE_WEB_API_KEY},
-                      data=payload)
-    return r.json()
-
-
-def get_user_info(UID):
-    user_doc_ref = db.collection(u'users').document(UID)
-    user_doc = user_doc_ref.get()
-    return user_doc.to_dict()
-
-
-def set_user_info(UID, element, new_value):
-    doc_ref = db.collection(u'users').document(UID)
-    field_updates = {element.value: new_value}
-    doc_ref.update(field_updates)
-
-
 class FirebaseEnum(Enum):
     BIRTH = "birth"
     CURRENT_GOAL = "current_goal"
@@ -66,27 +24,87 @@ class FirebaseEnum(Enum):
     WEIGHT = "weight"
 
 
-# ------------------- Create Account -------------------
-user_email = "riskgrant@gmail.com"
-user_password = "123456"
-# create_firebase_account(user_email, user_password)
+class FirebaseConnector:
+    def create_firebase_account(email: str, password: str):
+        """
+        Creates a user account with the given email and password
+        :param email: user email
+        :param password: user password
+        :return: none
+        """
+        user = firebase_admin.auth.create_user(email=email, password=password)
+        userUID = user.uid
+        data = {
+            u'birth': "",
+            u'current_goal': "",
+            u'email': email,
+            u'gender': "",
+            u'height': "",
+            u'name': "",
+            u'weight': "",
+        }
+        db.collection(u'users').document(userUID).set(data)
 
-# ------------------- Signing in -------------------
-firebase_user = sign_in_with_email_and_password(user_email, user_password)
-# If there is no user this is returned: User sign in response: {'error': {'code': 400, 'message': 'EMAIL_NOT_FOUND',
-# 'errors': [{'message': 'EMAIL_NOT_FOUND', 'domain': 'global', 'reason': 'invalid'}]}}
-print(f"User sign in response: {firebase_user}")
-userUID = firebase_user['localId']
-print(f"User UID in database: {userUID}")
-user_document = get_user_info(userUID)
-print(f"Retrieving initial user document: {user_document}")
+    def sign_in_with_email_and_password(email: str, password: str):
+        """
+        Signs in a user account with the given email and password
+        :param email: user email
+        :param password: user password
+        :return: dictionary response of user object from firebase
+        """
+        payload = json.dumps({
+            "email": email,
+            "password": password,
+            "returnSecureToken": "true"
+        })
+        r = requests.post(rest_api_url,
+                          params={"key": FIREBASE_WEB_API_KEY},
+                          data=payload)
+        return r.json()
 
-# ------------------- Making Changes -------------------
-new_email = "risk@gmail.com"
-set_user_info(userUID, FirebaseEnum.EMAIL, new_email)
+    def get_user_info(UID: str):
+        """
+        Retrieves a user's document given a specific UID
+        :param UID: users UID
+        :return: dictionary response of user's document from firebase
+        """
+        user_doc_ref = db.collection(u'users').document(UID)
+        user_doc = user_doc_ref.get()
+        return user_doc.to_dict()
 
-new_name = "Grant Risk"
-set_user_info(userUID, FirebaseEnum.NAME, new_name)
+    def set_user_info(UID: str, element: Enum, new_value: str):
+        """
+        Updates a user's information given a specific point in user document
+        :param UID: user UID
+        :param element: FirebaseEnum enum
+        :param new_value: new value being stored in user document
+        :return: None
+        """
+        doc_ref = db.collection(u'users').document(UID)
+        field_updates = {element.value: new_value}
+        doc_ref.update(field_updates)
 
-user_document = get_user_info(userUID)
-print(f"Retrieving modified user document: {user_document}")
+# # ------------------- Create Account -------------------
+# user_email = "riskgrant@gmail.com"
+# user_password = "123456"
+# # create_firebase_account(user_email, user_password)
+#
+# # ------------------- Signing in -------------------
+# firebase_user = FirebaseConnector.sign_in_with_email_and_password(user_email, user_password)
+# # If there is no user this is returned: User sign in response: {'error': {'code': 400, 'message': 'EMAIL_NOT_FOUND',
+# # 'errors': [{'message': 'EMAIL_NOT_FOUND', 'domain': 'global', 'reason': 'invalid'}]}}
+# print(f"User sign in response: {firebase_user}")
+# userUID = firebase_user['localId']
+# print(f"User UID in database: {userUID}")
+# user_document = FirebaseConnector.get_user_info(userUID)
+# print(f"Retrieving initial user document: {user_document}")
+#
+# # ------------------- Making Changes -------------------
+# new_email = "risk@gmail.com"
+# FirebaseConnector.set_user_info(userUID, FirebaseEnum.EMAIL, new_email)
+#
+# new_name = "Grant"
+# FirebaseConnector.set_user_info(userUID, FirebaseEnum.NAME, new_name)
+#
+# user_document = FirebaseConnector.get_user_info(userUID)
+# print(f"Retrieving modified user document: {user_document}")
