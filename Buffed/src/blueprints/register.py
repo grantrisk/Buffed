068e61ec853055c393a5f11a52a33d2aff6ff421
Tabs Chemelli <tabchemelli@hotmail.com>
2022-04-11 22:31:26 -0400

    Restructured profile questions, removed extra forms

--- Buffed/src/blueprints/register.py
+++ Buffed/src/blueprints/register.py
@@ -1,111 +1,125 @@
 from flask import Blueprint, render_template, request
 
-from forms import AccountSetupForm, ProfileQuestionnaire, DietQuestionnaire
+from forms import ProfileQuestionnaire
 
+# TODO: need to disable navbar use in setup screen, or move this to index's register module
 register_page = Blueprint("register", __name__, static_folder="static", template_folder="templates")
-
-
-# TODO: replace with code for setup_questions here or..?
-@register_page.route('/register')
-def register():
-    """
-    Render html and pass setup form to html
-    """
-    profile_form = ProfileQuestionnaire()
-    diet_form = DietQuestionnaire()
-    setup_form = AccountSetupForm()
-
-    # return render_template('register.html', profile_form=profile_form, diet_form=diet_form)
-    return render_template('register.html', setup_form=setup_form)
 
 
 def send_info(result):
     """
     Save info to the user object, then send to Firebase connector
     """
-    # TODO: Send info to User object to send to Firebase connector?
+    # TODO: Send info to User object to send to Firebase connector
     print(result)
 
 
-# TODO: need to get info and print it/confirm submission works
-# TODO: need to disable navbar use in setup screen
-# we can do this all at once
-@register_page.route('/account_setup', methods=['GET', 'POST'])
-def setup_questions():
+@register_page.route('/register', methods=['GET', 'POST'])
+def register():
     """
-    Renders both profile and diet questionnaires and gets validated posted info
+    Render html and pass setup form to html
     """
-    setup_form = AccountSetupForm()
+
+    profile_form = ProfileQuestionnaire()
 
     # validate_on_submit checks for submission with POST method,
     # then calls validate() to trigger form validation
-    if "submit" in request.form and setup_form.profile_q.validate(setup_form) and setup_form.diet_q.validate(setup_form):
-        # if a form is posted, store information as result
+    if profile_form.validate_on_submit():
+        # if form is posted, store information as result
         setup_result = {'sex': request.form["sex"],
                         'bday': request.form["bday"],
                         'weight': request.form["weight"],
                         'height': request.form["height"],
-                        'activity_level': request.form["activity_level"],
-                        'goal': request.form["goal"],  # are we using goal objects still?
-                        'diet_type': request.form["diet_type"],
-                        'allergies': request.form["allergies"]}
+                        'activity_lvl': request.form["activity_lvl"],
+                        'diet_type': profile_form.diet_type.data}
         # Send this result so it can be stored
-        print(setup_result)
         send_info(setup_result)
         # resume as normal (re-render page with forms after sending)
-        return render_template('register.html', setup_form=setup_form)
+        return render_template('dashboard.html')
     else:
         # render template with questionnaire forms
-        return render_template('register.html', setup_form=setup_form)
+        return render_template('register.html', profile_form=profile_form)
 
 
-# TODO: not using this for registration currently; should be moved where we'll handle editing information
-@register_page.route('/register-profile', methods=['GET', 'POST'])
-def profile_questions():
-    """
-    This method returns the account setup page.
-    :return: The account setup page including questionnaire forms
-    """
-    profile_form = ProfileQuestionnaire()
-    diet_form = DietQuestionnaire()
-    # validate_on_submit checks for submission with POST method,
-    # then calls validate() to trigger form validation
-    if profile_form.validate_on_submit():
-        # if a form is posted, store information as result
-        profile_result = {'sex': request.form["sex"],
-                          'bday': request.form["bday"],
-                          'weight': request.form["weight"],
-                          'height': request.form["height"],
-                          'activity_level': request.form["activity_level"],
-                          'goal': request.form["goal"]}
-        # Send this result so it can be stored
-        send_info(profile_result)
-        # resume as normal (re-render page with forms after sending)
-        return render_template('register.html', profile_form=profile_form, diet_form=diet_form)
-    else:
-        # render template with questionnaire forms
-        return render_template('register.html', profile_form=profile_form, diet_form=diet_form)
 
 
-# TODO: not using this for registration currently; should be moved where we'll handle editing information
-@register_page.route('/register-diet', methods=['GET', 'POST'])
-def diet_questions():
-    """
-    This method returns the account setup page.
-    :return: The account setup page including questionnaire forms
-    """
-    profile_form = ProfileQuestionnaire()
-    diet_form = DietQuestionnaire()
-
-    # validate_on_submit checks for submission with POST method,
-    # then calls validate() to trigger form validation
-    if diet_form.validate_on_submit():
-        diet_result = {'diet_type': request.form["diet_type"],
-                       'allergies': request.form["allergies"]}
-        # Send this result so it can be stored
-        send_info(diet_result)
-        # resume as normal (re-render page with forms after sending)
-        return render_template('register.html', profile_form=profile_form, diet_form=diet_form)
-    else:
-        # render template with questionnaire forms
-        return render_template('register.html', profile_form=profile_form, diet_form=diet_form)
+# we can do this all at once
+# @register_page.route('/account_setup', methods=['GET', 'POST'])
+# def setup_questions():
+#     """
+#     Renders both profile and diet questionnaires and gets validated posted info
+#     """
+#     setup_form = AccountSetupForm()
+#
+#     # validate_on_submit checks for submission with POST method,
+#     # then calls validate() to trigger form validation
+#     if "submit" in request.form and setup_form.profile_q.validate(setup_form) and setup_form.diet_q.validate(setup_form):
+#         # if a form is posted, store information as result
+#         setup_result = {'sex': request.form["sex"],
+#                         'bday': request.form["bday"],
+#                         'weight': request.form["weight"],
+#                         'height': request.form["height"],
+#                         'activity_level': request.form["activity_level"],
+#                         'goal': request.form["goal"],  # are we using goal objects still?
+#                         'diet_type': request.form["diet_type"],
+#                         'allergies': request.form["allergies"]}
+#         # Send this result so it can be stored
+#         print(setup_result)
+#         send_info(setup_result)
+#         # resume as normal (re-render page with forms after sending)
+#         return render_template('register.html', setup_form=setup_form)
+#     else:
+#         # render template with questionnaire forms
+#         return render_template('register.html', setup_form=setup_form)
+#
+#
+# # TODO: not using this for registration currently; should be moved where we'll handle editing information
+# @register_page.route('/register-profile', methods=['GET', 'POST'])
+# def profile_questions():
+#     """
+#     This method returns the account setup page.
+#     :return: The account setup page including questionnaire forms
+#     """
+#     profile_form = ProfileQuestionnaire()
+#     diet_form = DietQuestionnaire()
+#     # validate_on_submit checks for submission with POST method,
+#     # then calls validate() to trigger form validation
+#     if profile_form.validate_on_submit():
+#         # if a form is posted, store information as result
+#         profile_result = {'sex': request.form["sex"],
+#                           'bday': request.form["bday"],
+#                           'weight': request.form["weight"],
+#                           'height': request.form["height"],
+#                           'activity_level': request.form["activity_level"],
+#                           'goal': request.form["goal"]}
+#         # Send this result so it can be stored
+#         send_info(profile_result)
+#         # resume as normal (re-render page with forms after sending)
+#         return render_template('register.html', profile_form=profile_form, diet_form=diet_form)
+#     else:
+#         # render template with questionnaire forms
+#         return render_template('register.html', profile_form=profile_form, diet_form=diet_form)
+#
+#
+# # TODO: not using this for registration currently; should be moved where we'll handle editing information
+# @register_page.route('/register-diet', methods=['GET', 'POST'])
+# def diet_questions():
+#     """
+#     This method returns the account setup page.
+#     :return: The account setup page including questionnaire forms
+#     """
+#     profile_form = ProfileQuestionnaire()
+#     diet_form = DietQuestionnaire()
+#
+#     # validate_on_submit checks for submission with POST method,
+#     # then calls validate() to trigger form validation
+#     if diet_form.validate_on_submit():
+#         diet_result = {'diet_type': request.form["diet_type"],
+#                        'allergies': request.form["allergies"]}
+#         # Send this result so it can be stored
+#         send_info(diet_result)
+#         # resume as normal (re-render page with forms after sending)
+#         return render_template('register.html', profile_form=profile_form, diet_form=diet_form)
+#     else:
+#         # render template with questionnaire forms
+#         return render_template('register.html', profile_form=profile_form, diet_form=diet_form)
--- Buffed/src/forms.py
+++ Buffed/src/forms.py
@@ -1,10 +1,21 @@
-import wtforms
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, TextAreaField, SubmitField, SelectField, DateField, DecimalField, \
-    RadioField, SelectMultipleField
-from wtforms.validators import DataRequired, Email
+    RadioField, SelectMultipleField, widgets
+from wtforms.validators import DataRequired, Email, Regexp
 
 from Buffed.src.models import Goal
+
+
+class MultiCheckboxField(SelectMultipleField):
+    """
+    Checkbox version of multiselect field
+
+    Iterating the field will produce subfields, allowing custom rendering of
+    the enclosed checkbox fields.
+    More on this: https://wtforms.readthedocs.io/en/3.0.x/specific_problems/#specialty-field-tricks
+    """
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
 
 
 class ContactForm(FlaskForm):
@@ -27,43 +38,57 @@ class ProfileQuestionnaire(FlaskForm):
     """
     Form for profile questionnaire
     """
-    # what does sex/gender do for us? do we care for calories?
-    sex = SelectField("Sex", [DataRequired()], choices=["Male", "Female", "Other"])
+    # Personal Questionnaire
+    # TODO: need to fix validation for bday, weight, and height ; need to show error messages
+    sex = RadioField("Sex", [DataRequired()], choices=["Male", "Female"])
     bday = DateField("Birthdate", [DataRequired()])
-    weight = DecimalField("Weight in", [DataRequired()])
-    height = DecimalField("Height", [DataRequired()])
+    weight = DecimalField("Weight (lb)", [DataRequired()])
+    height = StringField('Height (ft)',
+                         [DataRequired(), Regexp('[3-7]\'\d{1,2}', message='Wrong height format. Example: 5\'7')])
     activity_lvl = RadioField("Activity Level", [DataRequired()], choices=["Athletic", "Moderate", "Sedentary"])
-    # TODO: are we still using goal objects or..?
-    goal = Goal("0", "Default Goal", True, 1300, {'protein': 100, 'carbs': 140, 'fat': 100}, 3, 110)
+
+    # Goal Information: Based on Goal model
+    # goal = Goal("0", "Default Goal", True, 1300, {'protein': 100, 'carbs': 140, 'fat': 100}, 3, 110)
+
+    # Diet Information: Based on Health Labels
+    diet_type = MultiCheckboxField("Diet Type",
+                                   choices=['Vegan', 'Vegetarian', 'Pescatarian', 'Dairy-Free',
+                                            'Gluten-Free', 'Wheat-Free', 'Egg-Free', 'Peanut-Free',
+                                            'Tree-Nut-Free', 'Soy-Free', 'Fish-Free', 'Shellfish-Free',
+                                            'Pork-Free', 'Red-Meat-Free', 'Crustacean-Free', 'Celery-Free',
+                                            'Mustard-Free', 'Sesame-Free', 'Lupine-Free', 'Mollusk-Free',
+                                            'Alcohol-Free', 'No oil added', 'FODMAP-Free', 'Kosher'])
 
     submit = SubmitField("Save")
 
-
-class DietQuestionnaire(FlaskForm):
-    """
-    Form for diet questionnaire
-    """
-    # TODO: This is a draft; need to check Edamam what categories return; add food keyword search?
-    diet_type = SelectMultipleField("Diet Type", [DataRequired()],
-                                    choices=["Vegan", "Vegetarian", "Mediterranean",
-                                             "Pescatarian", "Kosher", "Gluten-free",
-                                             "Ketogenic", "Other", "None"])
-
-    allergies = SelectMultipleField("Allergies", [DataRequired()],
-                                    choices=["Peanuts", "Nut oils", "Tree nuts", "Wheat",
-                                             "Soybean/Soy", "Milk/Dairy", "Egg",
-                                             "Fish", "Shellfish", "Sesame", "Potato",
-                                             "Chocolate / Cacao", "Other", "None"])
-
-    submit = SubmitField("Save")
-
-
-class AccountSetupForm(FlaskForm):
-    """
-    Form that combines the profile and diet forms
-    """
-    # we can combine both forms into one for the account set up,
-    # while preserving their individual forms for future use
-    profile_q = wtforms.FormField(ProfileQuestionnaire)
-    diet_q = wtforms.FormField(DietQuestionnaire)
-
+#
+# class DietQuestionnaire(FlaskForm):
+#     """
+#     Form for diet questionnaire
+#     """
+#     # TODO: This is a draft; need to check Edamam what categories return; add food keyword search?
+#     diet_type = MultiCheckboxField("Diet Type", [DataRequired()],
+#                                    choices=['Vegan', 'Vegetarian', 'Pescatarian', 'Dairy-Free',
+#                                             'Gluten-Free', 'Wheat-Free', 'Egg-Free', 'Peanut-Free',
+#                                             'Tree-Nut-Free', 'Soy-Free', 'Fish-Free', 'Shellfish-Free',
+#                                             'Pork-Free', 'Red-Meat-Free', 'Crustacean-Free', 'Celery-Free',
+#                                             'Mustard-Free', 'Sesame-Free', 'Lupine-Free', 'Mollusk-Free',
+#                                             'Alcohol-Free', 'No oil added', 'FODMAP-Free', 'Kosher'])
+#
+#     allergies = MultiCheckboxField("Allergies", [DataRequired()],
+#                                    choices=["Peanuts", "Nut oils", "Tree nuts", "Wheat",
+#                                             "Soybean/Soy", "Milk/Dairy", "Egg",
+#                                             "Fish", "Shellfish", "Sesame", "Potato",
+#                                             "Chocolate / Cacao", "Other", "None"])
+#
+#     submit = SubmitField("Save")
+#
+#
+# class AccountSetupForm(FlaskForm):
+#     """
+#     Form that combines the profile and diet forms
+#     """
+#     # we can combine both forms into one for the account set up,
+#     # while preserving their individual forms for future use
+#     profile_q = wtforms.FormField(ProfileQuestionnaire)
+#     diet_q = wtforms.FormField(DietQuestionnaire)
