20f1610cebcae6f6e5723cc79da1433b6b233fef
Cody Jackson <clj1175@uncw.edu>
2022-04-18 11:08:46 -0400

    Added Active Goal to the dashboard and created functionality within the modal pop-up to set active goal.

--- Buffed/src/blueprints/dashboard.py
+++ Buffed/src/blueprints/dashboard.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 import firebase_connector as fb
-from models import Goal, Meal
-
+from models import *
 dashboard_page = Blueprint("dashboard", __name__, static_folder="static", template_folder="templates")
 
 
@@ -11,8 +10,13 @@ dashboard_page = Blueprint("dashboard", __name__, static_folder="static", templa
 def dashboard():
     UID = current_user.get_id()
     goalList = fb.get_user_goals(UID)
+    for goal in goalList:
+        if goal['is_active']:
+            active_goal = goal
+
+
     """
         This method returns the page for the dashboard.
         :return: render_template('dashboard.html')
         """
-    return render_template('dashboard.html', goals=goalList)
+    return render_template('dashboard.html', goals=goalList, goal=active_goal)
--- Buffed/src/blueprints/my_goals.py
+++ Buffed/src/blueprints/my_goals.py
@@ -10,9 +10,6 @@ from datetime import date
 from models import Goal
 
 my_goals_page = Blueprint("my_goals", __name__, static_folder="static", template_folder="templates")
-# Temporary Direct Access to Firestore
-db = firestore.client()
-
 
 
 def generate_goal_id():
@@ -58,14 +55,7 @@ def my_goals():
         This method returns the page my_goals as well as the goals per user.
         :return: render_template('my_goals.html'), goals
     """
-    # Temporary Hard-Coded UserID
-    # UID = "VS8kx0L0DJVXhBEev86LyxUZpZb2"
     UID = current_user.get_id()
-
-    def get_user_goal_collection():
-        goal_doc_ref = db.collection(u'users').document(UID).collection(u'savedGoals')
-        return goal_doc_ref
-
     if request.method == "POST":
         goal_id = str(generate_goal_id())
         goal_name = request.form.get("goal_name")
@@ -76,32 +66,27 @@ def my_goals():
         number_of_meals = int(request.form.get("number_of_meals"))
         desired_weight = int(request.form.get("desired_weight"))
         new_goal = Goal(goal_id, goal_name, is_active, calories, macro_nutrients, number_of_meals, desired_weight)
-        # Testing standard goal creation for sign up
-        # create_standard_goal()
         fb.create_user_new_goal(UID, new_goal)
 
-    # Update Goal
-    # goal_doc_ref.update({u'is_active': True})
-
     goalList = fb.get_user_goals(UID)
-    # Temporarily deleting old goals
-    # FirebaseConnector.delete_user_goal(UID, u'06270fac-be35-44d6-a2bc-c6e5646aba88')
     return render_template('my_goals.html', goals=goalList)
 
 
 @my_goals_page.route('/update_goals/', methods=["POST"])
 def update_my_goals():
-
     UID = current_user.get_id()
     if request.form['action'] == "Update":
         goal_id = request.form.get("goal_id")
         goal_name = request.form.get("goal_name")
-        is_active = True
         calories = int(request.form.get("calories"))
         macro_nutrients = {"carbs": [int(request.form.get("carbs"))], "fat": [int(request.form.get("fat"))],
                            "protein": [int(request.form.get("protein"))]}
         number_of_meals = int(request.form.get("number_of_meals"))
         desired_weight = int(request.form.get("desired_weight"))
+        if request.form.get("active_checkbox") is not None:
+            is_active = True
+        else:
+            is_active = False
         updated_goal = Goal(goal_id, goal_name, is_active, calories, macro_nutrients, number_of_meals, desired_weight)
         # Testing standard goal creation for sign up
         # create_standard_goal()
