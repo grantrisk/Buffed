417806c2b73d6aae3f98c52aabc048442819c412
Blake Jackson <baj8533@uncw.edu>
2022-04-20 19:30:32 -0400

    Functional form added to edit profile.  Might do some tinkering with it so that the user doesn't have to enter every single field to change their data.  Also, some fields to the profile page will now show the users data.

--- Buffed/src/blueprints/edit_profile.py
+++ Buffed/src/blueprints/edit_profile.py
@@ -1,12 +1,65 @@
-from flask import Blueprint, render_template
+from flask_login import current_user
+from flask import Blueprint, render_template, request, redirect, url_for
+
+import firebase_connector as fb
+from firebase_connector import FirebaseEnum
+
+from forms import ProfileQuestionnaire
+from blueprints import my_goals
 
 edit_profile_page = Blueprint("edit_profile", __name__, static_folder="static", template_folder="templates")
 
 
-@edit_profile_page.route('/')
+def send_info(result, UID):
+    """
+    Save info to the user object, then send to Firebase connector
+    """
+    print("Sending to FB: ", result)
+
+    # how do I get a current user's id?
+    # UID = User.get_id()
+    # UID = "aTgX2eI0XLN6CGPiGRacjTOM8g32"  # email: fake.email@email.com // pass: 123456
+
+    fb.set_user_info(UID, FirebaseEnum.NAME, result.get('name'))
+    fb.set_user_info(UID, FirebaseEnum.GENDER, result.get('sex'))
+    fb.set_user_info(UID, FirebaseEnum.BIRTH, result.get('birth'))
+    fb.set_user_info(UID, FirebaseEnum.WEIGHT, result.get('weight'))
+    fb.set_user_info(UID, FirebaseEnum.HEIGHT, result.get('height'))
+    fb.set_user_info(UID, FirebaseEnum.ACTIVITY, result.get('activity_lvl'))
+    fb.set_user_info(UID, FirebaseEnum.CURRENT_GOAL, result.get('current_goal'))
+    fb.set_user_info(UID, FirebaseEnum.DIET, result.get('diet_type'))
+    my_goals.create_standard_goal(UID)
+
+def calculate_height(height):
+    # calculate height in feet
+    height = height.split("'")
+    return float(height[0]) + float(height[1]) / 12
+
+
+@edit_profile_page.route('/', methods=['GET', 'POST'])
 def edit_profile():
     """
         This method returns the page for my profile.
         :return: render_template('my_profile.html')
         """
-    return render_template('edit_profile.html')
+
+    UID = current_user.get_id()
+    profile_form = ProfileQuestionnaire()
+    # validate_on_submit checks for submission with POST method,
+    # then calls validate() to trigger form validation
+    if profile_form.validate_on_submit():
+        setup_result = {'name': request.form["name"],
+                        'sex': request.form["sex"],
+                        'birth': request.form["birth"],
+                        'weight': request.form["weight"],
+                        'height': calculate_height(request.form["height"]),
+                        'activity': request.form["activity"],
+                        'diet': profile_form.diet_type.data}
+        # Send this result so it can be stored
+        print("Passing on: ", setup_result)
+        send_info(setup_result, UID)
+
+        # Go to dashboard
+        return redirect(url_for("my_profile.my_profile"))
+
+    return render_template('edit_profile.html', profile_form=profile_form)
--- Buffed/src/blueprints/my_profile.py
+++ Buffed/src/blueprints/my_profile.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
-
+from flask_login import login_required, current_user
+import firebase_connector as fb
+import random
 my_profile_page = Blueprint("my_profile", __name__, static_folder="static", template_folder="templates")
 
 
@@ -11,4 +12,22 @@ def my_profile():
         This method returns the page for my profile.
         :return: render_template('my_profile.html')
         """
-    return render_template('my_profile.html')
+    UID = current_user.get_id()
+    user_info = fb.get_user_info(UID)
+
+    greeting_list = ['Howdy', 'Hello', 'Welcome', 'Welcome back', 'How\'s it going', 'Long-time no see',
+                'Yo', 'What\'s up', 'Heyyyyy', 'Sup']
+
+    greeting = greeting_list[random.randint(0, 9)]
+    print(user_info)
+    print("Greeting: ", greeting)
+    name = user_info['name']
+    weight = user_info['weight']
+    height = user_info['height']
+    email = user_info['email']
+    activity = user_info['activity']
+    gender = user_info['gender']
+    current_goal = user_info['current_goal']
+
+    return render_template('my_profile.html', name=name, weight=weight, height=height, email=email,
+                           activity=activity, gender=gender, goal=current_goal, greeting=greeting)
