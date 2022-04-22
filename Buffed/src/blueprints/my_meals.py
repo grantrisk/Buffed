bbcbdc56615f407df00d321cf7d198c1b58b4b01
Steven Allen <sja3869@uncw.edu>
2022-04-22 09:01:36 -0400

    Add functionality for adding meals to Today's Plan

--- Buffed/src/blueprints/my_meals.py
+++ Buffed/src/blueprints/my_meals.py
@@ -3,8 +3,11 @@ from flask_login import login_required, current_user
 
 import firebase_connector
 from edamam_connector import EdamamConnector
+from models import MealType
 
 my_meals_page = Blueprint("my_meals", __name__, static_folder="static", template_folder="templates")
+
+edamam_instance = EdamamConnector()
 
 
 @my_meals_page.route('/')
@@ -25,8 +28,10 @@ def add_to_todays_plan():
     Used for AJAX POST requests to save a given meal to Today's Plan
     :return: success status
     """
-    if 'id' in request.json:
-        firebase_connector.add_to_todays_plan(current_user.get_id(), request.json['id'])
+    if 'mealID' in request.form and 'mealTypeOption' in request.form:
+        meal = edamam_instance.get_recipe_by_id(request.form['mealID'])
+        meal.meal_type_section = request.form['mealTypeOption']
+        firebase_connector.add_meal_todays_plan(current_user.get_id(), meal)
         resp = jsonify(success=True)
     else:
         resp = jsonify(success=False)
